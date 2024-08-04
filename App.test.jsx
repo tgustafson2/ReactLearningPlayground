abf6f6120dbf3b33f2ffc39c64dbb1b3f3986e34
Thomas Gustafson <gus.duckfan@gmail.com>
2024-08-03 19:23:50 -0700

    Testing

---
+++ App.test.jsx
@@ -0,0 +1,21 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./src/App.jsx";
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    await user.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
---
+++ src/PrpTypes.jsx
@@ -0,0 +1,16 @@
+import PropType from "prop-types";
+import React from "react";
+
+const RenderName = (props) => {
+    return <div>{props.name}</div>;
+};
+
+RenderName.propTypes = {
+    name: PropTypes.string,
+};
+
+RenderName.defaultProps = {
+    name: "Zach",
+}
+
+export default RenderName;
